1d92496e5ff1fcc20325386b19f9c331a79961a6
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-06-06 18:16:27 -0400

    feature: detect active link

--- src/components/layout/NavHeader/NavHeader.jsx
+++ src/components/layout/NavHeader/NavHeader.jsx
@@ -1,27 +1,53 @@
 import { HashLink } from "react-router-hash-link";
+import { useLocation } from "react-router-dom";
 
 import "./NavHeader.css";
 
-//TODO: use NavHashLink so can get the active class stuff
+/* 
+  Issue: NavHashLink does not apply active styling anymore and this issue returned https://github.com/rafgraph/react-router-hash-link/issues/29. 
+  (I think had similar issues w/ React Router Dom NavLink.)
+  
+  SOLUTION: combined the following to detect active link w/ hash:
+  https://github.com/WebDevSimplified/react-navbar/blob/main/src/Navbar.js
+  https://github.com/WebDevSimplified/react-navbar/blob/main/src/styles.css
+  make sure anchor class after li class since inheriting color from above
+  https://medium.com/how-to-react/add-an-active-classname-to-the-link-using-react-router-b7c350473916
+  --> except used location.hash
+*/
+
+function CustomLink({ to, children, ...props }) {
+  const location = useLocation();
+
+  return (
+    <li className={location.hash === to ? "active" : ""}>
+      <HashLink to={to} {...props}>
+        {children}
+      </HashLink>
+    </li>
+  );
+}
+
 //TODO: make the brand link to /
+//TODO: smooth scrolling
+//TODO: mobile menu
 const NavHeader = () => {
   return (
     <div className="navHeader">
       <nav className="navBar">
         <h1 className="brandName">Simple Not Simpler</h1>
         <ul className="navMenu">
-          <li >
-            <HashLink className="navMenuLink"  to="#about">About</HashLink>
-          </li>
-          <li>
-            <HashLink className="navMenuLink"  to="#portfolio">Portfolio</HashLink>
-          </li>
-          <li>
-            <HashLink className="navMenuLink"  to="#skills">Skills</HashLink>
-          </li>
-          <li>
-            <HashLink className="navMenuLink"  to="#contact">Contact</HashLink>
-          </li>
+          <CustomLink to="#about" className="navMenuLink">
+            About
+          </CustomLink>
+          <CustomLink to="#portfolio" className="navMenuLink">
+            Portfolio
+          </CustomLink>
+          <CustomLink to="#skills" className="navMenuLink">
+            Skills
+          </CustomLink>
+          <CustomLink to="#contact" className="navMenuLink">
+            Contact
+          </CustomLink>
         </ul>
       </nav>
     </div>
