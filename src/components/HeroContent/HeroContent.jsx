d8ac006a9b1144d571a8b4e7017cac34885a8ebc
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-06-13 10:28:09 -0400

    chore: clean up

--- src/components/HeroContent/HeroContent.jsx
+++ src/components/HeroContent/HeroContent.jsx
@@ -1,10 +1,6 @@
 import Button from "../Button/Button.jsx";
 import "./HeroContent.css";
 
-//maybe button should be outside hero content?
-//or maybe I need a div for summary & button?
-//or just need left padding / margin on button?
-//TODO: maybe the content should be centered and just intro flex-start?
 const HeroContent = ({ intro, summary }) => {
   return (
     <div className="heroContent">
