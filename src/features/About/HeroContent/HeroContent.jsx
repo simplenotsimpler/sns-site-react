f8de9f16ac03e1ee611768e8b618fd779c5a58b7
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-07-31 08:36:45 -0400

    fix(About): keyboard tab order

--- src/features/About/HeroContent/HeroContent.jsx
+++ src/features/About/HeroContent/HeroContent.jsx
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 import ReactScrollLink from "../../../components/ReactScrollLink.jsx";
 
 //updated intro to h2 since WAVE Evaluation suggested it may be a heading
+//manually set tabIndex b/c wasn't correct order. probably due to mix of Link and ReactScrollLinks
+/* 
+  accessibility note: Firefox accessibility inspector said should have tabindex 0 but then on re-test complains about not being focusable :(
+  https://developer.mozilla.org/en-US/docs/Web/Accessibility/Understanding_WCAG/Keyboard?utm_source=devtools&utm_medium=a11y-panel-checks-keyboard#avoid_using_tabindex_attribute_greater_than_zero
+  https://developer.mozilla.org/en-US/docs/Web/Accessibility/Understanding_WCAG/Keyboard?utm_source=devtools&utm_medium=a11y-panel-checks-keyboard#clickable_elements_must_be_focusable_and_should_have_interactive_semantics
+  */
 const HeroContent = ({ intro, hero }) => {
   return (
     <div className="heroContent">
@@ -12,7 +18,7 @@ const HeroContent = ({ intro, hero }) => {
       <div className="summary">
         <p>
           {hero.lead} Please feel free to check out my{" "}
-          <ReactScrollLink to="portfolio" className="heroLink">
+          <ReactScrollLink to="portfolio" className="heroLink" tabIndex={0}>
             portfolio
           </ReactScrollLink>
           .
@@ -20,7 +26,7 @@ const HeroContent = ({ intro, hero }) => {
         <p>{hero.why}</p>
         <p>
           {hero.extra} Please feel free to browse my{" "}
-          <ReactScrollLink to="skills" className="heroLink">
+          <ReactScrollLink to="skills" className="heroLink" tabIndex={0}>
             skills
           </ReactScrollLink>{" "}
           and{" "}
@@ -29,6 +35,7 @@ const HeroContent = ({ intro, hero }) => {
             target="_blank"
             rel="noreferrer noopener"
             className="heroLink"
+            tabIndex={0}
           >
             resume
           </Link>
@@ -38,11 +45,15 @@ const HeroContent = ({ intro, hero }) => {
           <h2 className="ctaHeader">Let's Connect!!!</h2>
           <p className="ctaText">
             {hero.ctaIntro} Please feel free to email me at{" "}
-            <Link to="mailto:contact@simplenotsimpler.com" className="heroLink">
+            <Link
+              to="mailto:contact@simplenotsimpler.com"
+              className="heroLink"
+              tabIndex={0}
+            >
               contact@simplenotsimpler.com
             </Link>{" "}
             or drop me a line via one of my other{" "}
-            <ReactScrollLink to="contact" className="heroLink">
+            <ReactScrollLink to="contact" className="heroLink" tabIndex={0}>
               contact
             </ReactScrollLink>{" "}
             methods.{" "}
